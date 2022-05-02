41a850d81b108779eacff73803523e570373c984
sanya <sanec_290600@mail.ru>
2022-05-03 01:04:05 +1000

    msg

--- data_gen.py
+++
@@ -1,9 +0,0 @@
-import requests
-import random
-from time import sleep 
-
-while 1:
-    n = random.randint(1011,1019)
-    m = random.randint(91,99)
-    requests.get(f'http://localhost:5000/request_data?co_2={n}&t_voc={m}')
-    sleep(3.0)
