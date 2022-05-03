b886584e597d4a17bd162ec4bebb5e62f2541b58
sanya <sanec_290600@mail.ru>
2022-05-03 15:05:17 +1000

    msg

--- app/config.py
+++ app/config.py
@@ -4,7 +4,7 @@ class Configuration(object):
     DEBUG = True
     SECRET_KEY = 'wadqwqwrqvwteltw[wr[qq'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:password@localhost/esp_data'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:root@db:3306/esp_data'
     STATIC_FOLDER = 'static'
     JWT_TOKEN_LOCATION = 'cookies'
     JWT_COOKIE_CSRF_PROTECT = False
