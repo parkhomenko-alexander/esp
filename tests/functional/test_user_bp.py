7e8e09f0a7d57c01ae80577493c208afde955af1
sanya <sanec_290600@mail.ru>
2022-05-03 12:16:34 +1000

    msg

---
+++ tests/functional/test_user_bp.py
@@ -0,0 +1,11 @@
+def test_user_bp_index_with_fixture(test_client):
+    response = test_client.get('/user/')
+    data = response.data.decode("utf-8")
+    assert response.status_code == 200
+    assert 'Система мониторинга параметров воздуха' in data
+
+def test_user_bp_login_with_fixture(test_client):
+    response = test_client.get('/user/login')
+    data = response.data.decode("utf-8")
+    assert response.status_code == 200
+    assert 'Войти' in data
